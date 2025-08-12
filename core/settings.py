1d935968f6f4bd9b99a56657aa8783ae1a189edc
muhammedasher <ideasgeneratror@MacBook-Pro-Asher.local>
2025-08-12 18:14:03 +0500

    lang change and fix uz

--- core/settings.py
+++ core/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'uz'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
