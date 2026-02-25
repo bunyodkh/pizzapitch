d198321ce2d067ec525549c32f950b023190cec5
bunyodkh <bunyod.kh@gmail.com>
2026-02-25 12:46:55 +0500

    enable only standard reg

--- main/views.py
+++ main/views.py
@@ -10,5 +10,5 @@ def index(request):
     active_event = Event.objects.filter(is_active=True).first()
     mentors = Mentor.objects.filter(show=True).order_by('name')
     partners = Partner.objects.filter(show=True).order_by('name')
-    form = ParticipantRegistrationForm()
+    form = ParticipantRegistrationForm(initial={'registration_type': 'standard'})
     return render(request, 'index.html', {'active_event': active_event, 'form': form, 'mentors': mentors, 'partners': partners})
